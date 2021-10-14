ec1fa81e96337402b4ca2e982886292994c3c6e0
Pedro T. Pires <pedrotpoliveira@gmail.com>
2021-10-13 22:27:37 -0300

    feat: added Posts model

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Post(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, db_column="author"
+    )
+    title = models.CharField(max_length=120, blank=True, default="")
+    content = models.TextField()
+    slug = models.SlugField()
+
+    class Meta:
+        ordering = ["created"]
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return f"/{self.slug}/"
